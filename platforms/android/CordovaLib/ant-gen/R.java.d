1880ba66e757a55ed838581d8faeb47ee2bbf3bc
Loïc Knuchel <loicknuchel@gmail.com>
2014-04-02 12:40:01 +0200

    add phonegap files

---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : /home/loic/workspace/projets/retail-scan/ionicApp/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ platforms/android/ant-gen/org/knuchel/retailscan/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.knuchel.retailscan;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
