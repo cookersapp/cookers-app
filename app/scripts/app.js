9da92e5b12d141c4357b3bbc69c40f5fc14a7ef6
Loïc Knuchel <loicknuchel@gmail.com>
2014-09-11 13:39:43 +0200

    move to ionic-beta12

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ionic', 'ionic.contrib.ui.cards', 'ngSanitize', 'ngAnimate', 'ngTouch', 'ngCordova', 'firebase', 'angular-md5', 'monospaced.elastic', 'pasvaz.bindonce'])
+angular.module('app', ['ionic', 'ngSanitize', 'ngAnimate', 'ngTouch', 'ngCordova', 'firebase', 'angular-md5', 'monospaced.elastic', 'pasvaz.bindonce'])
 
 .config(function($stateProvider, $urlRouterProvider, $provide){
   'use strict';
