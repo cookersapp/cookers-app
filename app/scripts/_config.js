f54969362815d18fb7929f001c1ffefc69bf48b3
Loïc Knuchel <loicknuchel@gmail.com>
2014-10-09 22:03:50 +0200

    set backend urls

--- app/scripts/_config.js
+++ app/scripts/_config.js
@@ -6,8 +6,9 @@ var Config = (function(){
     track: true,
     debug: true
   };
-  var devBackendUrl = 'http://dev-cookers.herokuapp.com/';
-  var prodBackendUrl = 'http://cookers.herokuapp.com/';
+  var localBackendUrl = 'http://localhost:9000';
+  var devBackendUrl = 'http://dev-cookers.herokuapp.com';
+  var prodBackendUrl = 'http://cookers.herokuapp.com';
   cfg.backendUrl = cfg.debug ? devBackendUrl : prodBackendUrl;
 
   return cfg;
